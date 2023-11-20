f8e88b82d83193b2361e99837cab92856af0feeb
haoptimizer <nguyen_duy_ha@optimizer.co.jp>
2023-11-20 08:28:27 +0700

    Complete code m_1

--- 1/index.js
+++ 1/index.js
@@ -4,5 +4,40 @@ let availableKeywords = [
     "Easy tutorials",
     "Web design tutorials",
     "Javascript",
-    "Where to learn coding online"
-]
+    "Where to learn coding online",
+    "Where to lern web design",
+    "How to create a website"
+];
+
+const resultBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+
+    if (input.length){  // If inputfield has value
+        // Filter result contain inputfield
+        result = availableKeywords.filter((keyword) => {
+            return keyword.toLocaleLowerCase().includes(input.toLocaleLowerCase());
+        });
+    }
+    display(result);
+
+    if (!result.length){
+        resultBox.innerHTML = "";
+    }
+}
+
+// Convert result to HTML format
+function display(result){
+    const content = result.map((list) => {
+        return "<li onclick=selectInput(this)>" + list + "</li>";
+    });
+
+    resultBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function selectInput(list){
+    inputBox.value = this.innerHTML;
+}
